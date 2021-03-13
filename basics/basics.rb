956bad33a8ab82180c6e456c0cb123e0e8bb5fc9
brenogcota <brenocota4@gmail.com>
2021-03-12 23:12:31 -0300

    init repository

---
+++ basics/basics.rb
@@ -0,0 +1,23 @@
+# Strings
+
+# declare variables
+name = "John"
+
+# similiar to template literals javascript
+message = "Welcome #{name}"
+
+# print message
+puts message
+# output: Welcome John
+
+message = 'Welcome #{name}'
+# output Welcome #{name}
+
+message = <<~Message
+            Welcome #{name}
+          Message
+
+message = %q(welcome #{name})
+# output Welcome #{name}
+message = %Q(welcome #{name})
+# output: Welcome John
